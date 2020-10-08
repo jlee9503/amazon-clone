3877723a31afef3df5262596b102c54204bafa43
jlee9503 <jungsu9503@gmail.com>
2020-10-08 00:51:13 -0400

    complete front-end part of login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,22 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
 	return (
 		<div className="login">
-			<img
-				src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
-				alt="amazon-logo"
-			/>
+			<Link to="/">
+				<img
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+					alt="amazon-logo"
+					className="login-logo"
+				/>
+			</Link>
 
 			<div className="login-container">
-				<h1>Sign Up</h1>
+				<h1>Sign-In</h1>
 
 				<form>
-					<h5>Email</h5>
+					<h5>E-mail</h5>
 					<input type="text" />
+
+					<h5>Password</h5>
 					<input type="password" />
+
+					<button>Sign In</button>
 				</form>
+
+				<p>
+					By signing-in you agree to the Amazon Clone Project conditions of use
+					& sale. Please see our Privacy Notice, our Cookies Notice and our
+					Interest-Based Ads Notice.
+				</p>
+
+				<button className="sign-up-button">Create your Amazon Account</button>
 			</div>
 		</div>
 	);
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,48 +1,47 @@
 import React from "react";
 import "./Product.css";
-import { useStateValue } from './StateProvider';
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
-  const [{ basket }, dispatch] = useStateValue();
-  
+	const [{ basket }, dispatch] = useStateValue();
+
 	const addToBasket = () => {
 		// dispatch the item into the data layer
 		dispatch({
 			type: "ADD_TO_BASKET",
-      item: {
-        id: id,
-        title: title,
-        image: image,
-        price: price,
-        rating: rating,
+			item: {
+				id: id,
+				title: title,
+				image: image,
+				price: price,
+				rating: rating,
 			},
 		});
-  };
-  
-  return (
-    <div className="product">
-      <div className="product-info">
-        <p className="info-description">{title}</p>
-        <p className="product-price">
-          <small>$</small>
-          <strong>{price}</strong>
-        </p>
+	};
 
-        <div className="product-rating">
-          {Array(rating).fill().map((_) => (
-            <p>⭐</p>
-          ))}
-        </div>
-      </div>
+	return (
+		<div className="product">
+			<div className="product-info">
+				<p className="info-description">{title}</p>
+				<p className="product-price">
+					<small>$</small>
+					<strong>{price}</strong>
+				</p>
 
-      <img
-        src={image}
-        alt="product-img"
-      />
+				<div className="product-rating">
+					{Array(rating)
+						.fill()
+						.map((_, idx) => (
+							<p key={idx}>⭐</p>
+						))}
+				</div>
+			</div>
 
-      <button onClick={addToBasket}>Add to Basket</button>
-    </div>
+			<img src={image} alt="product-img" />
+
+			<button onClick={addToBasket}>Add to Basket</button>
+		</div>
 	);
 }
 
-export default Product
+export default Product;
